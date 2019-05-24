c8e4eda672158fdbd716cb95a1d1db3fb246ff1c
Voon <macbookair@Macbooks-MacBook-Air.local>
2019-05-25 02:21:29 +0800

    Doc Live

---
+++ src/components/DocLive.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div id="data"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DocPreview",
+  data() {
+    content: "";
+    sample:{
+        name:"Barack Obama",
+        invoiceNum:"123456",
+        houseNumber:"123",
+        street:"White House",
+        amount:"USD 100"
+    }
+  },
+  created() {},
+  methods: {
+    getLiveDoc() {
+      this.$apollo
+        .query({
+          query: getDoc(this.$route.params.id, "LIVE")
+        })
+        .then(data => {
+          this.content = data.data.getDoc.content;
+        });
+    },
+    putDataIntoTemplate(){
+        
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
